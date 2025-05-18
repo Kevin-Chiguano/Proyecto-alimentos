3a84ead2dd69aca3dabead5625c83efe3e2a99a2
Kevin <kevin64dj@gmail.com>
2025-05-17 20:12:34 -0500

    Creacion de front y back
    se creo las carpetas front y back

---
+++ BackNode/index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('¡Backend funcionando!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
